c401b9d5b364e067b142012d1dee614e456e9221
repak <repak@ROHITH>
2024-07-13 19:34:24 +0530

    TL-- final commit

---
+++ GITMavenProj01/src/main/java/com/nt/service/NetBanking.java
@@ -0,0 +1,9 @@
+package com.nt.service;
+
+public class NetBanking {
+	
+	public  String  netBanking(String uname,String pass) {
+		return "netbanking is added";
+	}
+
+}
---
+++ GITMavenProj01/src/test/java/com/nt/test/AppTest.java
@@ -0,0 +1,33 @@
+package com.nt.test;
+
+import static org.junit.Assert.assertEquals;  // Importing the assertEquals method
+import org.junit.Test;
+
+import com.nt.service.Arithmetic;
+
+public class AppTest {
+
+    @Test
+    public void testWithPositives() {
+        Arithmetic ar = new Arithmetic();
+        int expected = 50;
+        int actual = ar.sum(30, 20);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testWithNegatives() {
+        Arithmetic ar = new Arithmetic();
+        int expected = -100;
+        int actual = ar.sum(-50, -50);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testWithZeros() {
+        Arithmetic ar = new Arithmetic();
+        int expected = 0;
+        int actual = ar.sum(0, 0);
+        assertEquals(expected, actual);
+    }
+}
