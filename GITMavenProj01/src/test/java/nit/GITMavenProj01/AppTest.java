99062c9c7a8cc19dabcdd5214cc82b442a2958ca
repak <repak@ROHITH>
2024-07-13 18:44:47 +0530

    initial project commit

---
+++ GITMavenProj01/src/test/java/nit/GITMavenProj01/AppTest.java
@@ -0,0 +1,20 @@
+package nit.GITMavenProj01;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
